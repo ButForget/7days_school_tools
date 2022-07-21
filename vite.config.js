36c67e4a2a0116734fc64c467c787239fbdf679a
CaK <butforget@outlook.com>
2022-07-21 14:51:14 +0800

    fix: *

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+/*
+ * @Author: CaK butforget@outlook.com
+ * @Date: 2022-06-12 22:13:36
+ * @LastEditors: CaK butforget@outlook.com
+ * @LastEditTime: 2022-07-21 14:51:03
+ * @FilePath: \ct7_vue-3\vite.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: "/7day_school_tools",
 })
